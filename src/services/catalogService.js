5069f894ab4c0170d853dda710d6982100cf378f
SKuncheva <120223698+SKuncheva@users.noreply.github.com>
2023-04-28 01:54:34 +0300

    wrong uploaded

--- src/services/catalogService.js
+++
@@ -1,9 +0,0 @@
-import * as request from './requester';
-
-
-export const products = async () => {
-  const res = await request.get('http://localhost:3030/data/products');
-  const products=Object.values(res);
-  // console.log(products);
-  return products;
-};
